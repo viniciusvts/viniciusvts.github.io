ef695bc8d6d3ec8c90e86ecdd20f26d7ef8cd8b7
Vinicius de Santana <vinicius.vts@gmail.com>
2017-08-16 19:51:08 -0300

    adicionei links e imagens para os links

--- vtsantana/src/app/home/home.component.ts
+++ vtsantana/src/app/home/home.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  mylinks = {
+    linkedin : 'https://www.linkedin.com/in/viniciusvts/',
+    github : 'https://github.com/viniciusvts',
+    facebook : 'https://www.facebook.com/viniciusvts',
+    mail : 'dev@vtsantana.com.br'
+  };
 
   constructor() { }
 
