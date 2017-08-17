e9f6d2997c034bfbdec1630e9ea53efa871e9fe6
Vinicius de Santana <vinicius.vts@gmail.com>
2017-08-16 23:32:03 -0300

    adcionando conteúdo

--- vtsantana/src/app/home/home.component.ts
+++ vtsantana/src/app/home/home.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  mylinks = {
-    linkedin : 'https://www.linkedin.com/in/viniciusvts/',
-    github : 'https://github.com/viniciusvts',
-    facebook : 'https://www.facebook.com/viniciusvts',
-    mail : 'dev@vtsantana.com.br'
+  links = {
+    'linkedin' : 'https://www.linkedin.com/in/viniciusvts/',
+    'github' : 'https://github.com/viniciusvts',
+    'facebook' : 'https://www.facebook.com/viniciusvts',
+    'email' : 'mailto:dev@vtsantana.com.br'
   };
 
   constructor() { }
