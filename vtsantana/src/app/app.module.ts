2eada31030dc0e7d7f963fb2d677ce3ac079082a
Vinicius de Santana <vinicius.vts@gmail.com>
2017-08-20 12:08:31 -0300

    instalação e importação do componente angular material

--- vtsantana/src/app/app.module.ts
+++ vtsantana/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjetosComponent } from './projetos/projetos.component';
 import { BlogComponent } from './blog/blog.component';
+
+import { MdCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,8 @@ import { BlogComponent } from './blog/blog.component';
     BlogComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MdCardModule,
     BrowserModule
   ],
   providers: [],
