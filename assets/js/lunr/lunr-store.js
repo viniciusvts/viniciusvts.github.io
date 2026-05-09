90af4fd904ddd6b5bea7999b05a3367b874f9aef
viniciusvts <viniciusvts@users.noreply.github.com>
2026-05-09 21:03:27 +0000

    deploy: c80a269fe36fc74b40bcd337da6cd8d96cb01024

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,37 @@
 var store = [{
         "title": "A reflection about time",
-        "excerpt":"Time is money. (image by Lukas Blazek on Unsplash) Dear reader, I hope you are well and succeeding in managing your time. I believe most people know the saying: “Time is money,” and I think there are more reasons to believe in this statement. We are paid for the time...","categories": [],
+        "excerpt":"Time is money. (image by Lukas Blazek on Unsplash) Dear reader, I hope you are well and succeeding in managing your time. I believe most people know the saying: “Time is money,” and I think there are many reasons why this is true. We are paid for the time we...","categories": ["en"],
         "tags": [],
         "url": "/blog/a-reflection-about-time/",
         "teaser": null
       },{
+        "title": "Uma reflexão sobre o tempo",
+        "excerpt":"Tempo é dinheiro. (imagem by Lukas Blazek on Unsplash) Caro leitor, espero que esteja bem e obtendo êxito no gerenciamento do seu tempo. Acredito que maioria conhece a máxima: “Tempo é dinheiro” e considero possível ter mais motivos para acreditar na afirmação. Recebemos pelo tempo que passamos trabalhando, afinal estamos...","categories": ["pt-br"],
+        "tags": [],
+        "url": "/blog/uma-reflexao-sobre-o-tempo/",
+        "teaser": null
+      },{
         "title": "Dominating the conversion of SVN repositories to GIT",
-        "excerpt":"Plan and Make. (Image by Brett Jordan on Unsplash) In the agile work environment I’m in, there were times between sprints when I found myself without immediate tasks. Instead of letting this idle time go unnoticed, I decided to use it productively. As a professional committed to my growth and...","categories": [],
+        "excerpt":"Plan and Make. (Image by Brett Jordan on Unsplash) In the agile work environment I’m in, there were times between sprints when I found myself without immediate tasks. Instead of letting this idle time go unnoticed, I decided to use it productively. As a professional committed to my growth and...","categories": ["en"],
         "tags": [],
         "url": "/blog/dominating-the-conversion-from-SVN-to-GIT/",
         "teaser": null
       },{
-        "title": "Converting an SVN repository to GIT: a practical guide",
-        "excerpt":"Conversion of source code managers. (image by DALL-E) Converting an SVN repository to a GIT repository is a process that involves migrating the data, structure, and history from one version control system to another. In the previous article, I discussed my research and in-depth study of converting SVN repositories to...","categories": [],
+        "title": "Utilizando o tempo livre para dominar a conversão de repositórios SVN para GIT",
+        "excerpt":"Planejar e fazer. (Imagem by Brett Jordan on Unsplash) No ambiente de trabalho ágil em que estou inserido, surgiram momentos entre as sprints em que me vi sem tarefas imediatas. Ao invés de deixar esse tempo ocioso passar despercebido, decidi aproveitá-lo de maneira produtiva. Como profissional comprometido com meu crescimento...","categories": ["pt-br"],
+        "tags": [],
+        "url": "/blog/dominando-a-conversao-de-repositorios-SVN-para-GIT/",
+        "teaser": null
+      },{
+        "title": "Converting an SVN repository to Git: a practical guide",
+        "excerpt":"Conversion of source code managers. (image by DALL-E) Converting an SVN repository to a GIT repository is a process that involves migrating the data, structure, and history from one version control system to another. In the previous article, I discussed my research and in-depth study of converting SVN repositories to...","categories": ["en"],
         "tags": [],
         "url": "/blog/the-guide-from-GIT-to-SVN-conversion/",
+        "teaser": null
+      },{
+        "title": "Conversão de repositório SVN para GIT: um guia prático",
+        "excerpt":"Conversão de gerenciadores de código. (imagem por DALL-E) A conversão de um repositório SVN para um repositório GIT é um processo que envolve a migração dos dados, da estrutura e do histórico de um sistema de controle de versão para outro. No artigo anterior abordei a minha pesquisa e aprofundamento...","categories": ["pt-br"],
+        "tags": [],
+        "url": "/blog/o-guia-da-conversao-de-GIT-para-SVN/",
         "teaser": null
       }]
